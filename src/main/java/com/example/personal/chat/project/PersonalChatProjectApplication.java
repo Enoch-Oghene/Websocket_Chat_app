fb5694eba57b4a9a6505bb4b75bf69adcdde3b2c
MAC <mac@MACs-MacBook-Air.local>
2021-12-15 11:15:03 +0100

    first commit

---
+++ src/main/java/com/example/personal/chat/project/PersonalChatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.personal.chat.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalChatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalChatProjectApplication.class, args);
+	}
+
+}
